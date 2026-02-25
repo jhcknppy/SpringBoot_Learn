00578c230d3cf66f8f348ea75c16c5aff5b10076
panpengyu <2840872812@qq.com>
2026-02-25 16:47:03 +0800

    create project

---
+++ src/test/java/com/ppy/SpringBootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
